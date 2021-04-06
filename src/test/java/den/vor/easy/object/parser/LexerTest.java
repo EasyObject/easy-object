b0eb71bcb61599ab66db2de60746868192dc6497
Danila Vorotyntsev <VorotyntsevDanila@gmail.com>
2021-04-06 21:35:10 +0300

    Refactored lexer (#10)
    Refactored lexer

---
+++ src/test/java/den/vor/easy/object/parser/LexerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020-2021 Danila Varatyntsev
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package den.vor.easy.object.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static den.vor.easy.object.parser.TokenType.EOF;
+import static den.vor.easy.object.parser.TokenType.INT_NUMBER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LexerTest {
+
+    @Test
+    void shouldReturnEmptyTokenHolder_whenNoTokenizerSupportsSymbol() {
+        TokenHolder holder = new Lexer(" ").tokenize();
+
+        Token token = holder.get(0);
+        assertEquals(token.getType(), EOF, () -> "Expected lexer to return an empty token holder, got " + holder);
+    }
+
+    @Test
+    void shouldReturnNonEmptyTokenHolder_whenTokenizerSupportsSymbol() {
+        TokenHolder holder = new Lexer("1").tokenize();
+
+        Token token = holder.get(0);
+        assertEquals(token.getType(), INT_NUMBER,
+                () -> "Expected lexer to return an empty token holder, got " + holder);
+    }
+}
